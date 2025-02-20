af92f15c66c64af88f7bd42a67e5844a005e45e9
gfpardal <goncalopardaleco@gmail.com>
2025-02-20 18:47:51 +0000

    v0.18

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
-let link = document.getElementById("cv-button");
-link.href = "CurriculoGP_EN.pdf"
+document.getElementById("cv-button").href = "CurriculoGP_EN.pdf"
