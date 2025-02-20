25c741709266a24da526e585bd60d214d4e521eb
gfpardal <goncalopardaleco@gmail.com>
2025-02-20 18:06:44 +0000

    v0.16

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const blob = document.getElementById("blob");
-
-  window.onpointermove = event => {
-    const {
-      clientX,
-      clientY
-    } = event;
-
-
-    // EDIT
-    // An offset to match the css 34vmax
-    const offset = (innerHeight < innerWidth ? innerHeight : innerWidth) * 0.34
-    blob.style.left = `${clientX - offset}px`
-    blob.style.top = `${clientY - offset}px`
-  };
-});
+document.getElementById("year").innerHTML = new Date().getFullYear();
