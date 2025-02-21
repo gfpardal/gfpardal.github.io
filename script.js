29d9713adfd4f934133bc22b454ad592a3111355
gfpardal <goncalopardaleco@gmail.com>
2025-02-21 09:08:05 +0000

    v0.26

--- script.js
+++ script.js
@@ -2,12 +2,44 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 var language = 'en';
 
-function loadLanguage() {
-    document.getElementById("cv-button").href = 
-        language === 'en' ? "CurriculumGP_EN.pdf" : "CurriculoGP_PT.pdf";
-    document.getElementById("greeting").innerHTML = 
-        language === 'en' ? "HELLO, I'M" : "OLA, EU SOU O";
+const translations = {
+    en: {
+        greeting: "HELLO, I'M",
+        description: "A Passionate ",
+        home: "Home",
+        about: "About",
+        projects: "Projects",
+        experience: "Experience",
+        contact: "Contact",
+        cvLink: "CurriculumGP_EN.pdf"
+    },
+    pt: {
+        greeting: "OLÁ, EU SOU O",
+        description: "Um aficionado",
+        home: "Início",
+        about: "Sobre",
+        projects: "Projetos",
+        experience: "Experiência",
+        contact: "Contactos",
+        cvLink: "CurriculoGP_PT.pdf"
+    }
+};
+
+function loadLanguage(language) {
+    const langData = translations[language];
+
+    document.getElementById("greeting").innerHTML = langData.greeting;
+    document.getElementById("description").innerHTML = langData.description;
+
+    /** Sidebar Languages **/
+    document.getElementById("home").innerHTML = langData.home;
+    document.getElementById("about").innerHTML = langData.about;
+    document.getElementById("projects").innerHTML = langData.projects;
+    document.getElementById("experience").innerHTML = langData.experience;
+    document.getElementById("contact").innerHTML = langData.contact;
+    document.getElementById("cv-button").href = langData.cvLink;
 }
+
 
 function changeLanguage() {
     language = language === 'en' ? 'pt' : 'en';
